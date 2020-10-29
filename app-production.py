a549ab141918233c452fddd74063660dbd1ce1bc
sotola <sotolatopiga@gmail.com>
2020-10-30 02:22:08 +0700

    v 1.13

--- app-production.py
+++ app-production.py
@@ -1,56 +1,16 @@
 from Test import loadData_2020_10_28
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import numpy as np
-from utilities import *
-# from receiveData import *
 from common import mmap, dump, threading_func_wrapper
 from Context import Context
-from werkzeug.wrappers import Request, Response
 import json, pickle
-
-from requestHandler import handleHoseDataPost
-
+from werkzeug.serving import run_simple
+from requestHandler import handleHoseDataPost, handlePsDataPost, handlePsDataGet, handleHoseDataGet
 
 app = Flask(__name__)
 CORS(app)
 
-#############################################################################################################
-
-@app.route('/api/phaisinh-snapshot-inbound', methods=['GET', 'POST', 'DELETE', 'PUT'])
-def psListener():
-    from flask import request
-    return
-
-
-@app.route('/')
-def index():
-    a = {'name': 'Sarah', 'age': 34, 'isEmployed': True}
-    return json.dumps(a)
-
-
-@app.route('/api/hose-indicators-outound', methods=['GET', 'POST', 'DELETE', 'PUT'])
-def hose_indicators_outound():
-    o.maybeUpdatedicators()
-    return jsonify(o.indicators)
-
-
-@app.route('/api/hose-snapshot-inbound', methods=['GET', 'POST', 'DELETE', 'PUT'])
-def hose_snapshot_inbound():
-    from flask import request
-    timeObj, timeStr, data, indicators = handleHoseDataPost(request)
-    indicators['i'] = len(o.history)
-    o.addToHistory({'time': timeObj, 'parsed': data})
-    o.indicators.append(indicators)
-    print(f"\r#### {len(o.history)} ####", end="")
-    return jsonify(indicators) # return jsonify(res)
-
-
-@app.route("/shutdown")
-def shutdown():
-    shutdown_server()
-    return "Successfully closed server"
-
+############################################# STATIC HTML PAGES ##################################$###########
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -58,5 +18,51 @@ def shutdown_server():
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
+
+# Shut down server via http
+@app.route("/shutdown")
+def shutdown():
+    shutdown_server()
+    return "Successfully closed server"
+
+
+# Home page
+@app.route('/')
+def index():
+    a = {'name': 'Sarah', 'age': 34, 'isEmployed': True}
+    return json.dumps(a)
+
+
+########################################## OUTBOUND DATA SERVICES ###########################################
+
+# Return hose indicators for charts
+@app.route('/api/hose-indicators-outound', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def hose_indicators_outbound():
+    return handleHoseDataGet(o)
+
+
+# Return phai sinh OHLC data for charts
+@app.route('/api/ps-ohlc-outbound', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def ps_ohlc_outbound():
+    return handlePsDataGet(o)
+
+########################################### INBOUND INGESTION ################################################
+
+# Ingest phai sinh data
+@app.route('/api/ps-snapshot-inbound', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def ps_snapshot_inbound():
+    return jsonify(handlePsDataPost(o))
+
+
+# Ingest Hose data
+@app.route('/api/hose-snapshot-inbound', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def hose_snapshot_inbound():
+    timeObj, timeStr, data, indicators = handleHoseDataPost(o)
+    indicators['i'] = len(o.history)
+    o.addToHistory({'time': timeObj, 'parsed': data})
+    o.indicators.append(indicators)
+    return jsonify(indicators) # return jsonify(res)
+
+###############################################################################################################
 
 o = Context()
